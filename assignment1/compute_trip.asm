4eaf1f96e49944dc1a0092068a5f89217e024f4d
KelseyDang2002 <kelseydang813@gmail.com>
2023-08-18 10:14:04 -0700

    Files created

---
+++ assignment1/compute_trip.asm
@@ -0,0 +1,5 @@
+; Name: Kelsey Dang
+; Email: kdangdo2002@csu.fullerton.edu
+; Due Date: 10/4/23
+; Subject: CPSC 240-03 Assignment 1
+; Filename: compute_trip.asm
---
+++ assignment1/main.cpp
@@ -0,0 +1,23 @@
+// Name: Kelsey Dang
+// Email: kdangdo2002@csu.fullerton.edu
+// Due Date: 10/4/23
+// Subject: CPSC 240-03 Assignment 1
+// Filename: main.cpp
+
+#include <iostream>
+
+using namespace std;
+
+// Driver Code
+int main() {
+  cout << "Welcome to Trip Advisor by Kelsey Dang.\n";
+  cout << "We help you plan your trip.\n";
+
+  // Call assembly module
+
+  // Output number received
+  cout << "The main module received this number __ and will keep it for a while.\n";
+  cout << "A zero will be sent to your operating system.\n";
+  cout << "Good-bye. Have a great trip.\n";
+  return 0
+}
