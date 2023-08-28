08dc8a9e7473483db15dfb0e1e5dca05aadada71
KelseyDang2002 <kelseydang813@gmail.com>
2023-08-27 20:51:51 -0700

    seg fault after 3 inputs

--- assignment1/compute_trip.asm
+++ assignment1/compute_trip.asm
@@ -47,30 +47,29 @@ section .text
 las_vegas:                ; start here
 
   ; Backup GPRs
-  ; push rbp
-  ; mov rbp, rsp
-  ; push rbx
-  ; push rcx
-  ; push rdx
-  ; push rsi
-  ; push rdi
-  ; push r8
-  ; push r9
-  ; push r10
-  ; push r11
-  ; push r12
-  ; push r13
-  ; push r14
-  ; push r15
-  ; pushf
+  push rbp
+  mov rbp, rsp
+  push rbx
+  push rcx
+  push rdx
+  push rsi
+  push rdi
+  push r8
+  push r9
+  push r10
+  push r11
+  push r12
+  push r13
+  push r14
+  push r15
+  pushf
 
   ; Prompt for input of initial distance
-  push qword 0
+  push qword 0            ; push to top of stack
   mov qword rax, 0
   mov rdi, stringform     ; "%s"
   mov rsi, initial_msg
   call printf
-  pop rax
   ; End of block
 
   ; Get initial from user
@@ -83,19 +82,19 @@ las_vegas:                ; start here
   pop rax                 ; restore stack
   ; End of block
 
-  ; Print initial from user
-  mov rax, 0
-  mov rsi, initial_output
-  call printf
-  ; End of block
-
+  ; ; Print initial from user
+  ; mov rax, 0
+  ; mov rsi, initial_output
+  ; call printf
+  ; ; End of block
+  ;
   ; Prompt for input of miles maintained
   mov qword rax, 0
   mov rdi, stringform     ; "%s"
   mov rsi, miles_msg
   call printf
   ; End of block
-
+  ;
   ; Get miles from user
   push qword 0
   mov qword rax, 0
@@ -105,13 +104,13 @@ las_vegas:                ; start here
   movsd xmm9, [rsp]
   pop rax
   ; End of block
-
-  ; Print miles from user
-  mov rax, 0
-  mov rsi, miles_output
-  call printf
-  ; End of block
-
+  ;
+  ; ; Print miles from user
+  ; mov rax, 0
+  ; mov rsi, miles_output
+  ; call printf
+  ; ; End of block
+  ;
   ; Prompt for input of final speed
   mov qword rax, 0
   mov rdi, stringform     ; "%s"
@@ -128,49 +127,50 @@ las_vegas:                ; start here
   movsd xmm10, [rsp]
   pop rax
   ; End of block
-
-  ; Print final_seg from user
-  mov rax, 0
-  mov rsi, final_output
-  call printf
-  ; End of block
-
-  ; Block to copy 253.5 into xmm1 and 3.0 into xmm2
-  movsd xmm1, qword [hotel_distance]
-  movsd xmm2, qword [three]
-
-  ; Output avg_speed_msg
-  mov rax, 0
-  mov rdi, avg_speed_msg
-  call printf
-  ; End of block
-
-  ; Output total travel time
-  mov rax, 0
-  mov rdi, total_msg
-  call printf
-  ; End of block
+  ;
+  ; ; Print final_seg from user
+  ; mov rax, 0
+  ; mov rsi, final_output
+  ; call printf
+  ; ; End of block
+  ;
+  ; ; Block to copy 253.5 into xmm1 and 3.0 into xmm2
+  ; movsd xmm1, qword [hotel_distance]
+  ; movsd xmm2, qword [three]
+  ;
+  ; ; Output avg_speed_msg
+  ; mov rax, 0
+  ; mov rdi, avg_speed_msg
+  ; call printf
+  ; ; End of block
+  ;
+  ; ; Output total travel time
+  ; mov rax, 0
+  ; mov rdi, total_msg
+  ; call printf
+  ; ; End of block
 
   ; Block to set return value
+setreturnvalue:
   push r14
   movsd xmm0, [rsp]
   pop r14
 
   ; Restore GPRs
-  ; popf
-  ; pop r15
-  ; pop r14
-  ; pop r13
-  ; pop r12
-  ; pop r11
-  ; pop r10
-  ; pop r9
-  ; pop r8
-  ; pop rdi
-  ; pop rsi
-  ; pop rdx
-  ; pop rcx
-  ; pop rbx
-  ; pop rbp
+  popf
+  pop r15
+  pop r14
+  pop r13
+  pop r12
+  pop r11
+  pop r10
+  pop r9
+  pop r8
+  pop rdi
+  pop rsi
+  pop rdx
+  pop rcx
+  pop rbx
+  pop rbp
 
   ret
